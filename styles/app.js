ca5342c44d2b24259172e6be301a821c5a1f13af
Fernando Beckworth <nandoughcorp@live.com>
2020-11-12 17:04:51 -0500

    work_from_home

---
+++ styles/app.js
@@ -0,0 +1,7 @@
+document.addEventListener("click",showlist);
+
+function showlist(){
+    var nlist = document.getElementsByClassName(".nav").childNodes
+    if (nlist = "flex")Then 
+        nlist.style.display = "none"
+}
